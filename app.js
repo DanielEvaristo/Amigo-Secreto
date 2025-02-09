cfcd43809bf92f0352599801da21cccc5c2d1ec6
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2025-02-08 23:10:53 -0600

    Creación de función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Challenge amigos secreto
+
+let amigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo");
+
+    //Verificar que el campo no esté vacío
+    if(!nombre.value.trim()){
+        alert("Por favor, inserte un nombre");
+        nombre.value = "";
+        return;
+    }
+
+    //Verificar que el amigo no haya sido agregado
+    for(let i = 0; i<amigos.length; i++){
+        if(amigos[i] === nombre.value){
+            alert("Este amigo ya fue agregado");
+            nombre.value = "";
+            return;
+        }
+    }
+
+    //Agregar el nombre a la lista de amigos
+    amigos.push(nombre.value);
+    nombre.value = "";
+    console.log(amigos);
+}
